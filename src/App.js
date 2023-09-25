887fc15c8e41cca99f6a9f7900f1e761e1b1fab5
siri1515 <siri91445@gmail.com>
2023-09-25 20:46:13 -0300

    user can add new cardsets now

--- src/App.js
+++ src/App.js
@@ -2,63 +2,21 @@ import { React, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CardBlock from './Components/CardBlock';
 import CardSetPage from './Components/CardSetPage';
-//example
-const data = [
-  {
-    id: 1,
-    title: 'Card Set 1',
-    cards: [
-      {
-        id: 1,
-        question: 'Question 1',
-        answer: 'Answer 1',
-      },
-      {
-        id: 2,
-        question: 'Question 2',
-        answer: 'Answer 2',
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: 'Card Set 2',
-    cards: [
-      {
-        id: 1,
-        question: 'Question 1',
-        answer: 'Answer 1',
-      },
-      {
-        id: 2,
-        question: 'Question 2',
-        answer: 'Answer 2',
-      },
-      {
-        id: 3,
-        question: 'Question 3',
-        answer: 'Answer 3',
-      },
-    ],
-  },
-];
 
 function App() {
 
   const [chosenSet, setChosenSet] = useState('');
 
-  //from CardSetItem component
-  function getIdHandler2(id){
-    const newList = data.find((set) => {
-      return set.id === id;
-    })
-    setChosenSet(newList);
+  function saveChosenSetHandler(chosenSet){
+    setChosenSet(chosenSet);
   }
+  console.log("chosenset", chosenSet);
+
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<CardSetPage list={data} onCardSetItemClick2={getIdHandler2}/>} />
+          <Route path="/" element={<CardSetPage onSaveChosenSet={saveChosenSetHandler} />} />
           <Route path="/quizcard" element={<CardBlock list={chosenSet.cards} />} />
         </Routes>
       </div>
--- src/Components/CardSetPage.js
+++ src/Components/CardSetPage.js
@@ -1,21 +1,75 @@
+import {useState} from 'react';
 import CardSetItem from "./CardSetItem";
+import AddNewCardSet from './AddNewCardSet/AddNewCardSet';
 import './CardSetBlock.css';
 import { useNavigate } from 'react-router-dom';
 
+const data = [
+    {
+      id: 1,
+      title: 'Card Set 1',
+      cards: [
+        {
+          id: 1,
+          question: 'Question 1',
+          answer: 'Answer 1',
+        },
+        {
+          id: 2,
+          question: 'Question 2',
+          answer: 'Answer 2',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Card Set 2',
+      cards: [
+        {
+          id: 1,
+          question: 'Question 1',
+          answer: 'Answer 1',
+        },
+        {
+          id: 2,
+          question: 'Question 2',
+          answer: 'Answer 2',
+        },
+        {
+          id: 3,
+          question: 'Question 3',
+          answer: 'Answer 3',
+        },
+      ],
+    },
+];
+
 export default function CardSetPage(props){
+
+    const[list, setList] = useState(data);
+
+    function addCardSetHandler(newCardSet){
+        setList((prevCardSet) => {
+            return [newCardSet, ...prevCardSet];
+        })
+    }
+    console.log("updated list: ", list);
 
     const navigate = useNavigate();
 
     function getIdHandler(id){
-        props.onCardSetItemClick2(id);
+        const clickedSet = list.find((set) => {
+            return set.id === id;
+        })
+        props.onSaveChosenSet(clickedSet);
         navigate('/quizcard');
     }
 
     return(
         <div>
-            <AddNewCardSet />
+            <AddNewCardSet onAddCardSet={addCardSetHandler} />
             <div className="card_set_block">
-                {props.list.map((cardset) => {
+                {list.map((cardset) => {
                     return(
                         <CardSetItem 
                             title={cardset.title} 
